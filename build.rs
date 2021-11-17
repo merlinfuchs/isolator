2f1d02468e69968e2e5b6086356873ab006570e8
merlinfuchs <hi@merlin.gg>
2021-11-17 17:53:25 +0100

    allow loading additional js files during build

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::path::{PathBuf};
+use std::path::{Path, PathBuf};
 use deno_core::{JsRuntime, RuntimeOptions};
 
 fn build_protobuf() {
@@ -10,16 +10,43 @@ fn build_protobuf() {
         .unwrap();
 }
 
+fn get_js_files(dir: &str) -> Vec<PathBuf> {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    let mut js_files = std::fs::read_dir(dir)
+        .unwrap()
+        .map(|dir_entry| {
+            let file = dir_entry.unwrap();
+            manifest_dir.join(file.path())
+        })
+        .filter(|path| path.extension().unwrap_or_default() == "js")
+        .collect::<Vec<PathBuf>>();
+    js_files.sort();
+    js_files
+}
+
 fn create_runtime() -> JsRuntime {
     let extensions = vec![
         ext_resources::init()
     ];
 
-    let runtime = JsRuntime::new(RuntimeOptions {
+    let mut runtime = JsRuntime::new(RuntimeOptions {
         will_snapshot: true,
         extensions,
         ..Default::default()
     });
+
+    let display_root = Path::new(env!("CARGO_MANIFEST_DIR")).parent().unwrap();
+    for js_file in get_js_files("js") {
+        let display_path = js_file.strip_prefix(display_root).unwrap();
+        let display_path_str = display_path.display().to_string();
+        println!("cargo:rerun-if-changed={}", display_path_str);
+        runtime
+            .execute_script(
+                &("isolator:".to_string() + &display_path_str.replace('\\', "/")),
+                &std::fs::read_to_string(&js_file).unwrap(),
+            )
+            .unwrap();
+    }
 
     runtime
 }
